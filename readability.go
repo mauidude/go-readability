348ae3a93eef0a152d3f94ac40cbfab7823bc903
Shane Andrade <mauidude2@gmail.com>
2014-01-03 21:44:20 -0800

    initial commit

---
+++ readability.go
@@ -0,0 +1,539 @@
+package readability
+
+import (
+	"bytes"
+	"code.google.com/p/go.net/html"
+	"fmt"
+	"github.com/PuerkitoBio/goquery"
+	"log"
+	"math"
+	"regexp"
+	"strings"
+)
+
+var replaceBrsRegexp = regexp.MustCompile(`(?i)(<br[^>]*>[ \n\r\t]*){2,}`)
+var replaceFontsRegexp = regexp.MustCompile(`(?i)<(\/?)font[^>]*>`)
+
+var blacklistCandidatesRegexp = regexp.MustCompile(`(?i)popupbody`)
+var okMaybeItsACandidateRegexp = regexp.MustCompile(`(?i)and|article|body|column|main|shadow`)
+var unlikelyCandidatesRegexp = regexp.MustCompile(`(?i)combx|comment|community|disqus|extra|foot|header|menu|remark|rss|shoutbox|sidebar|sponsor|ad-break|agegate|pagination|pager|popup`)
+var divToPElementsRegexp = regexp.MustCompile(`(?i)<(a|blockquote|dl|div|img|ol|p|pre|table|ul)`)
+
+var negativeRegexp = regexp.MustCompile(`(?i)combx|comment|com-|contact|foot|footer|footnote|masthead|media|meta|outbrain|promo|related|scroll|shoutbox|sidebar|sponsor|shopping|tags|tool|widget`)
+var positiveRegexp = regexp.MustCompile(`(?i)rticle|body|content|entry|hentry|main|page|pagination|post|text|blog|story`)
+
+var normalizeWhitespaceRegexp = regexp.MustCompile(`[\r\n\f]+`)
+
+type candidate struct {
+	selection *goquery.Selection
+	score     float32
+}
+
+func (c *candidate) Node() *html.Node {
+	return c.selection.Get(0)
+}
+
+type Document struct {
+	input         string
+	document      *goquery.Document
+	content       string
+	candidates    map[*html.Node]*candidate
+	bestCandidate *candidate
+
+	RemoveUnlikelyCandidates bool
+	WeightClasses            bool
+	CleanConditionally       bool
+	BestCandidateHasImage    bool
+	RetryLength              int
+	MinTextLength            int
+	RemoveEmptyNodes         bool
+	WhitelistTags            []string
+}
+
+func NewDocument(s string) (*Document, error) {
+	d := &Document{
+		input:                    s,
+		WhitelistTags:            []string{"div", "p"},
+		RemoveUnlikelyCandidates: true,
+		WeightClasses:            true,
+		CleanConditionally:       true,
+		RetryLength:              250,
+		MinTextLength:            25,
+		RemoveEmptyNodes:         true,
+	}
+	err := d.initializeHtml(s)
+	if err != nil {
+		return nil, err
+	}
+
+	return d, nil
+}
+
+func (d *Document) initializeHtml(s string) error {
+	// replace consecutive <br>'s with p tags
+	s = replaceBrsRegexp.ReplaceAllString(s, "</p><p>")
+
+	// replace font tags
+	s = replaceFontsRegexp.ReplaceAllString(s, "<$1span>")
+
+	doc, err := goquery.NewDocumentFromReader(strings.NewReader(s))
+	if err != nil {
+		return err
+	}
+
+	// if no body (like from a redirect or empty string)
+	if doc.Find("body").Length() == 0 {
+		s = "<body/>"
+		return d.initializeHtml(s)
+	}
+
+	traverse(doc.Get(0), func(n *html.Node) {
+		if n.Type == html.CommentNode {
+			parent := n.Parent
+			if parent != nil {
+				parent.RemoveChild(n)
+			}
+		}
+	})
+
+	d.document = doc
+	return nil
+}
+
+func (d *Document) Content() string {
+	if d.content == "" {
+		d.prepareCandidates()
+
+		article := d.getArticle()
+		articleText := d.sanitize(article)
+
+		fmt.Println("Article===================")
+		fmt.Println(articleText)
+		fmt.Println("************END")
+
+		if len(strings.TrimSpace(articleText)) < d.RetryLength {
+			if d.RemoveUnlikelyCandidates {
+				d.RemoveUnlikelyCandidates = false
+			} else if d.WeightClasses {
+				d.WeightClasses = false
+			} else if d.CleanConditionally {
+				d.CleanConditionally = false
+			} else {
+				d.content = articleText
+			}
+
+			d.initializeHtml(d.input)
+			d.content = d.Content()
+		}
+
+		d.content = articleText
+	}
+
+	return d.content
+}
+
+func (d *Document) prepareCandidates() {
+	d.document.Find("script, style").Each(func(i int, s *goquery.Selection) {
+		removeNodes(s)
+	})
+
+	if d.RemoveUnlikelyCandidates {
+		d.removeUnlikelyCandidates()
+	}
+
+	d.transformMisusedDivsIntoParagraphs()
+	d.scoreParagraphs(d.MinTextLength)
+	d.selectBestCandidate()
+}
+
+func (d *Document) selectBestCandidate() {
+	var best *candidate
+
+	for _, c := range d.candidates {
+		if best == nil {
+			best = c
+		} else if best.score < c.score {
+			best = c
+		}
+	}
+
+	if best == nil {
+		best = &candidate{d.document.Find("body"), 0}
+	}
+
+	fmt.Println("Best candidate======")
+	fmt.Println(best.selection.Html())
+	fmt.Println("**************")
+	d.bestCandidate = best
+}
+
+func (d *Document) getArticle() string {
+	output := bytes.NewBufferString("<div>")
+
+	siblingScoreThreshold := float32(math.Max(10, float64(d.bestCandidate.score*.2)))
+
+	d.bestCandidate.selection.Siblings().Union(d.bestCandidate.selection).Each(func(i int, s *goquery.Selection) {
+		append := false
+		n := s.Get(0)
+
+		if n == d.bestCandidate.Node() {
+			append = true
+		} else if c, ok := d.candidates[n]; ok && c.score >= siblingScoreThreshold {
+			append = true
+		}
+
+		if s.Is("p") {
+			linkDensity := d.getLinkDensity(s)
+			content := s.Text()
+			contentLength := len(content)
+
+			if contentLength >= 80 && linkDensity < .25 {
+				append = true
+			} else if contentLength < 80 && linkDensity == 0 {
+				append, _ = regexp.MatchString(`\.( |$)`, content)
+			}
+		}
+
+		if append {
+			tag := "div"
+			if s.Is("p") {
+				tag = n.Data
+			}
+
+			html, _ := s.Html()
+			fmt.Fprintf(output, "<%s>%s</%s>", tag, html, tag)
+		}
+	})
+
+	output.Write([]byte("</div>"))
+
+	return output.String()
+}
+
+func (d *Document) removeUnlikelyCandidates() {
+	d.document.Find("*").Not("html,body").Each(func(i int, s *goquery.Selection) {
+		class, _ := s.Attr("class")
+		id, _ := s.Attr("id")
+
+		str := class + id
+
+		if blacklistCandidatesRegexp.MatchString(str) || (unlikelyCandidatesRegexp.MatchString(str) && !okMaybeItsACandidateRegexp.MatchString(str)) {
+			log.Printf("Removing unlikely candidate -%s\n", str)
+			removeNodes(s)
+		}
+	})
+}
+
+func (d *Document) transformMisusedDivsIntoParagraphs() {
+	d.document.Find("div").Each(func(i int, s *goquery.Selection) {
+		html, err := s.Html()
+		if err != nil {
+			log.Printf("Unable to transform div to p %s\n", err)
+			return
+		}
+
+		// transform <div>s that do not contain other block elements into <p>s
+		if !divToPElementsRegexp.MatchString(html) {
+			class, _ := s.Attr("class")
+			id, _ := s.Attr("id")
+			log.Printf("Altering div(#%s.%s) to p\n", id, class)
+
+			node := s.Get(0)
+			node.Data = "p"
+		}
+	})
+}
+
+func (d *Document) scoreParagraphs(minimumTextLength int) {
+	candidates := make(map[*html.Node]*candidate)
+
+	d.document.Find("p,td").Each(func(i int, s *goquery.Selection) {
+		text := s.Text()
+
+		// if this paragraph is less than x chars, don't count it
+		if len(text) < minimumTextLength {
+			return
+		}
+
+		parent := s.Parent()
+		parentNode := parent.Get(0)
+
+		grandparent := parent.Parent()
+		var grandparentNode *html.Node
+		if grandparent.Length() > 0 {
+			grandparentNode = grandparent.Get(0)
+		}
+
+		if _, ok := candidates[parentNode]; !ok {
+			candidates[parentNode] = d.scoreNode(parent)
+		}
+		if grandparentNode != nil {
+			if _, ok := candidates[grandparentNode]; !ok {
+				candidates[grandparentNode] = d.scoreNode(grandparent)
+			}
+		}
+
+		contentScore := float32(1.0)
+		contentScore += float32(strings.Count(text, ",") + 1)
+		contentScore += float32(math.Min(float64(int(len(text)/100.0)), 3))
+
+		candidates[parentNode].score += contentScore
+		if grandparentNode != nil {
+			candidates[grandparentNode].score += contentScore / 2.0
+		}
+	})
+
+	// scale the final candidates score based on link density. Good content
+	// should have a relatively small link density (5% or less) and be mostly
+	// unaffected by this operation
+	for _, candidate := range candidates {
+		candidate.score = candidate.score * (1 - d.getLinkDensity(candidate.selection))
+	}
+
+	d.candidates = candidates
+}
+
+func (d *Document) getLinkDensity(s *goquery.Selection) float32 {
+	linkLength := len(s.Find("a").Text())
+	textLength := len(s.Text())
+
+	if textLength == 0 {
+		return 0
+	}
+
+	return float32(linkLength) / float32(textLength)
+}
+
+func (d *Document) classWeight(s *goquery.Selection) int {
+	weight := 0
+	if !d.WeightClasses {
+		return weight
+	}
+
+	class, _ := s.Attr("class")
+	id, _ := s.Attr("id")
+
+	if class != "" {
+		if negativeRegexp.MatchString(class) {
+			weight -= 25
+		}
+
+		if positiveRegexp.MatchString(class) {
+			weight += 25
+		}
+	}
+
+	if id != "" {
+		if negativeRegexp.MatchString(id) {
+			weight -= 25
+		}
+
+		if positiveRegexp.MatchString(id) {
+			weight += 25
+		}
+	}
+
+	return weight
+}
+
+func (d *Document) scoreNode(s *goquery.Selection) *candidate {
+	contentScore := d.classWeight(s)
+	if s.Is("div") {
+		contentScore += 5
+	} else if s.Is("blockquote,form") {
+		contentScore = 3
+	} else if s.Is("th") {
+		contentScore -= 5
+	}
+
+	return &candidate{s, float32(contentScore)}
+}
+
+func (d *Document) sanitize(article string) string {
+	doc, err := goquery.NewDocumentFromReader(strings.NewReader(article))
+	if err != nil {
+		return ""
+	}
+
+	s := doc.Find("body")
+	doc.Find("h1,h2,h3,h4,h5,h6").Each(func(i int, header *goquery.Selection) {
+		if d.classWeight(s) < 0 || d.getLinkDensity(s) > 0.33 {
+			removeNodes(s)
+		}
+	})
+
+	doc.Find("form,object,iframe,embed").Each(func(i int, s *goquery.Selection) {
+		removeNodes(s)
+	})
+
+	if d.RemoveEmptyNodes {
+		doc.Find("p").Each(func(i int, s *goquery.Selection) {
+			html, _ := s.Html()
+			if len(strings.TrimSpace(html)) == 0 {
+				removeNodes(s)
+			}
+		})
+	}
+
+	d.cleanConditionally(s, "table,ul,div")
+
+	// we'll sanitize all elements using a whitelist
+	replaceWithWhitespace := map[string]bool{
+		"br":         true,
+		"hr":         true,
+		"h1":         true,
+		"h2":         true,
+		"h3":         true,
+		"h4":         true,
+		"h5":         true,
+		"h6":         true,
+		"dl":         true,
+		"dd":         true,
+		"ol":         true,
+		"li":         true,
+		"ul":         true,
+		"address":    true,
+		"blockquote": true,
+		"center":     true,
+	}
+
+	whitelist := make(map[string]bool)
+	for _, tag := range d.WhitelistTags {
+		tag = strings.ToLower(tag)
+		whitelist[tag] = true
+		delete(replaceWithWhitespace, tag)
+	}
+
+	var text string
+
+	doc.Union(s.Find("*")).Each(func(i int, s *goquery.Selection) {
+		if text != "" {
+			return
+		}
+
+		// if element is in whitelist, delete all its attributes
+		node := s.Get(0)
+		if _, ok := whitelist[node.Data]; ok {
+			node.Attr = make([]html.Attribute, 0)
+		} else {
+			// if root element, just use the text
+			if s.Parent().Length() == 0 {
+				text = s.Text()
+				return
+			} else {
+				// replace node with children
+				parent := node.Parent
+				var next *html.Node
+
+				for c := node.FirstChild; c != nil; c = next {
+					next = c.NextSibling
+					c.NextSibling = nil
+					c.PrevSibling = nil
+					c.Parent = nil
+
+					parent.InsertBefore(c, node)
+				}
+			}
+		}
+	})
+
+	if text == "" {
+		text, _ = d.document.Html()
+	}
+
+	return normalizeWhitespaceRegexp.ReplaceAllString(text, "\n")
+}
+
+func (d *Document) cleanConditionally(s *goquery.Selection, selector string) {
+	if !d.CleanConditionally {
+		return
+	}
+
+	s.Find(selector).Each(func(i int, s *goquery.Selection) {
+		node := s.Get(0)
+		weight := float32(d.classWeight(s))
+		contentScore := float32(0)
+
+		if c, ok := d.candidates[node]; ok {
+			contentScore = c.score
+		}
+
+		if weight+contentScore < 0 {
+			removeNodes(s)
+			log.Printf("Conditionally cleaned %s%s with weight %d and content score %d\n", node.Data, getName(s), weight, contentScore)
+			return
+		}
+
+		text := s.Text()
+		if strings.Count(text, ",") < 10 {
+			counts := map[string]int{
+				"p":     s.Find("p").Length(),
+				"img":   s.Find("img").Length(),
+				"li":    s.Find("li").Length() - 100,
+				"a":     s.Find("a").Length(),
+				"embed": s.Find("embed").Length(),
+				"input": s.Find("input").Length(),
+			}
+
+			contentLength := len(strings.TrimSpace(text))
+			linkDensity := d.getLinkDensity(s)
+			remove := false
+			reason := ""
+
+			if counts["img"] > counts["p"] {
+				reason = "too many images"
+				remove = true
+			} else if counts["li"] > counts["p"] && !s.Is("ul,ol") {
+				reason = "more <li>s than <p>s"
+				remove = true
+			} else if counts["input"] > int(counts["p"]/3.0) {
+				reason = "less than 3x <p>s than <input>s"
+				remove = true
+			} else if contentLength < d.MinTextLength && (counts["img"] == 0 || counts["img"] > 2) {
+				reason = "too short content length without a single image"
+				remove = true
+			} else if weight < 25 && linkDensity > 0.2 {
+				reason = fmt.Sprintf("too many links for its weight (%d)", weight)
+				remove = true
+			} else if weight >= 25 && linkDensity > 0.5 {
+				reason = fmt.Sprintf("too many links for its weight (%d)", weight)
+				remove = true
+			} else if (counts["embed"] == 1 && contentLength < 75) || counts["embed"] > 1 {
+				reason = "<embed>s with too short a content length, or too many <embed>s"
+				remove = true
+			}
+
+			if remove {
+				fmt.Printf("Conditionally cleaned %s with weight %d and content score %d because it has %s\n", getName(s), weight, contentScore, reason)
+				removeNodes(s)
+			}
+		}
+	})
+}
+
+func getName(s *goquery.Selection) string {
+	class, _ := s.Attr("class")
+	id, _ := s.Attr("id")
+
+	return fmt.Sprintf("#%s.%s", id, class)
+}
+
+func traverse(node *html.Node, eval func(*html.Node)) {
+	eval(node)
+
+	// check each child
+	for c := node.FirstChild; c != nil; c = c.NextSibling {
+		traverse(c, eval)
+	}
+}
+
+func removeNodes(s *goquery.Selection) {
+	s.Each(func(i int, s *goquery.Selection) {
+		parent := s.Parent()
+		if parent.Length() == 0 {
+			// TODO???
+		} else {
+			parent.Get(0).RemoveChild(s.Get(0))
+		}
+	})
+}
---
+++ readability_test.go
@@ -0,0 +1,166 @@
+package readability
+
+import (
+	"io/ioutil"
+	"strings"
+	"testing"
+)
+
+type expectedOutput struct {
+	requiredFragments []string
+	excludedFragments []string
+}
+
+func TestGeneralFunctionality(t *testing.T) {
+	html := `<html><head><title>title!</title></head><body><div><p>Some content</p></div></body>`
+	doc, err := NewDocument(html)
+	if err != nil {
+		t.Fatal("Unable to create document", err)
+	}
+
+	doc.MinTextLength = 0
+	doc.RetryLength = 1
+	content := doc.Content()
+	if content != "Some content" {
+		t.Errorf("Expected content %q to match %q", content, "Some content")
+	}
+}
+
+func TestIgnoringSidebars(t *testing.T) {
+	html := `html><head><title>title!</title></head><body><div><p>Some content</p></div><div class='sidebar'><p>sidebar<p></div></body>`
+	doc, err := NewDocument(html)
+	if err != nil {
+		t.Fatal("Unable to create document", err)
+	}
+
+	doc.MinTextLength = 0
+	doc.RetryLength = 1
+
+	content := doc.Content()
+	if strings.Contains(content, "sidebar") {
+		t.Errorf("Did not expect content %q to contain %q", content, "sidebar")
+	}
+}
+
+func TestInsertSpaceForBlockElements(t *testing.T) {
+	html := `<html><head><title>title!</title></head>
+          <body>
+            <div>
+              <p>a<br>b<hr>c<address>d</address>f</p>
+            </div>
+          </body>
+        </html>`
+
+	doc, err := NewDocument(html)
+	if err != nil {
+		t.Fatal("Unable to create document", err)
+	}
+
+	content := doc.Content()
+	if strings.Contains(content, "a b c d f") {
+		t.Errorf("Did not expect content %q to contain %q", content, "a b c d f")
+	}
+}
+
+func TestOutputForWellKnownDocuments(t *testing.T) {
+	inputs := map[string]*expectedOutput{
+		"blogpost_with_links.html": &expectedOutput{
+			requiredFragments: []string{
+				"The zebras and porcupines get together to beat the living shit out of zombies, who are trying to wreck the havoc upon them. The ceiling cat is awaken by the noise they're making, and summons the basement cat to do the punishment. Zombies bite the ceiling cat and ceiling cat decides to destroy the universe. Then the big bang happens and this shit doesn't matter anymore.",
+				"Ceiling cat, the",
+				"Basement cat, the",
+			},
+			excludedFragments: []string{},
+		},
+
+		"globemail-ottowa_cuts.html": &expectedOutput{
+			requiredFragments: []string{
+				"Treasury Board President Stockwell Day is trumpeting job cuts at government boards and agencies in the name of fiscal prudence – but the measures are largely phantom restraint because most affected posts are empty and have been for some time.",
+				"Mr. Day, the Harper government's point man for belt-tightening in Ottawa, released Monday a list of 245 cabinet appointments that will be eliminated to make government more efficient.",
+				"But 90 per cent of these positions are currently vacant. Many have been unfilled for years – and some for decades – often because the unused positions proved surplus to the needs of agencies or bodies.",
+				`“This looks more token than real,” Kevin Gaudet, federal director of the fiscally conservative Canadian Taxpayers Federation, said of the cuts.`,
+				"These reductions are the latest effort by the Conservative government, presiding over record deficits, to deliver two different messages at the same time. They want to show their political base they are keen on restraint but want to reassure Canadians in general that they are not slashing programs and services while the economic recovery is fragile.",
+				"“For now, they seem content to nip and tuck where it doesn't really hurt. The real restraint will begin in 2011, after the recovery has more fully taken root,” BMO Nesbitt Burns deputy chief economist Douglas Porter said.",
+				"At least 46 of the posts the Canadian government is eliminating have never been filled. These are jobs at boards and agencies that were created by past governments but never set up, including the Space Advisory Board, established in 1989 and eligible for 19 appointments by Ottawa.",
+				"“No one has ever sat on that board,” said Erik Waddell, director of communications for Industry Minister Tony Clement.",
+				"The same fate will befall 12 posts at the Canadian Council on the Status of the Artist – an organization created by a former Liberal government but never brought into being – and 15 positions at the Freshwater Fish Marketing Corporation's advisory committee, a panel that was never set up.",
+				"Other cuts are not cost savings but changes in paperwork. For instance, a senior government surveyor will be appointed to multiple boundary commissions at once instead of separately. And two top bureaucrats will continue to serve as chair and vice-chair of a body that manages the Employment Insurance program – but they won't be cabinet appointments any more.",
+				"Defending the measures, Mr. Day said the cuts matter even if the jobs are vacant now because it will prevent governments from ramping up the appointments down the road. “It's future savings,” he said.",
+				"If all the jobs had been filled, Ottawa would be saving a paltry $1.2-million a year in pay and salaries as a result of these appointment cuts. By comparison, the budget deficit for the year ending March 31 will be $53.8-billion.",
+				"But only 27 of the 245 jobs being cut are currently filled. A spokeswoman for Mr. Day said the estimated total savings from eliminating these 27 jobs is $53,000 to $62,000 in pay and $37,800 in travel bills.",
+				"The Treasury Board President – responsible for scrutinizing spending – said the exercise may not save much but demonstrates Ottawa can operate more leanly.",
+				"“It is what taxpayers want us to do, conduct the affairs of government and its services in an efficient way, and do it in a way that respects the taxpayers,” Mr. Day told the House of Commons.",
+				"The Official Opposition Liberals, however, called the move to cut mostly vacant posts an empty and hypocritical gesture. They charge the Tories have used their patronage powers to appoint dozens of supporters to many of the very same boards and agencies in the past 16 months.",
+				"“Since November 2008, 79 Conservatives who have donated $79,366.82 to the Conservative Party of Canada, Progressive Conservative Party, and the Canadian Alliance have been appointed to the same boards Minister Day singled out in his announcement today,” a release from Liberal critic Siobhan Coady said.",
+			},
+			excludedFragments: []string{
+				"Share with friends",
+				"Print or License",
+				// a commenter
+				"Hrmmm, rather pointless.",
+				// another comment
+				"Here's hoping the Conservatives react to the deficit",
+				"<SCRIPT",
+				"<script",
+			},
+		},
+		"channel4-1.html": &expectedOutput{
+			requiredFragments: []string{
+				"Judge rules Briton can be force-fed",
+				"A US judge has ruled that prison officials may continue force-feeding a Briton who began a hunger strike in September 2007 over claims he was convicted on a fabricated sexual assault charge.",
+				"William Coleman, reportedly originally from Liverpool, who is serving an eight-year sentence for rape, said he began his hunger strike to protest against a corrupt judicial system.",
+				"The state of Connecticut began force-feeding Coleman in September 2008 after he stopped accepting fluids, but he argued that the feedings violate his right of free speech.",
+			},
+			excludedFragments: []string{
+				"Share this article",
+			},
+		},
+		"foxnews-india1.html": &expectedOutput{
+			requiredFragments: []string{
+				"Police say 28 people have been killed in central India after the bus they were traveling in touched a high-voltage wire and caught fire.",
+				"Police officer Ram Pyari Dhurwey says the accident occurred Friday in Mandla district in Madhya Pradesh state.",
+				"It was the second such accident in India in as many days. At least 15 people were killed in eastern Bihar state on Thursday when they truck they were riding in touched a high-voltage wire.",
+			},
+			excludedFragments: []string{
+				"Leave a comment",
+				"Latest videos",
+			},
+		},
+	}
+
+	for file, expectedOutput := range inputs {
+		bytes, err := ioutil.ReadFile("test_fixtures/" + file)
+		if err != nil {
+			t.Fatal("Unable to read file test_fixtures/", file, err)
+		}
+
+		input := string(bytes)
+		doc, err := NewDocument(input)
+		if err != nil {
+			t.Fatal("Unable to create document", err)
+		}
+
+		content := doc.Content()
+		content = normalizeString(content)
+
+		for _, required := range expectedOutput.requiredFragments {
+			required = normalizeString(required)
+			if !strings.Contains(content, required) {
+				t.Errorf("Expected content %q to contain %q", content, required)
+			}
+		}
+
+		for _, excluded := range expectedOutput.excludedFragments {
+			excluded = normalizeString(excluded)
+			if strings.Contains(content, excluded) {
+				t.Errorf("Did not expect content %q to contain %q", content, excluded)
+			}
+		}
+	}
+}
+
+func normalizeString(s string) string {
+	s = strings.Replace(s, "&#39;", "'", -1)
+
+	return s
+}
